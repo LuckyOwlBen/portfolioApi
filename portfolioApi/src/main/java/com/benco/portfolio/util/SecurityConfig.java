747f0fca255f08d3506bce44c13411a3b57976b2
Ben <benmalmberg2@gmail.com>
2024-02-21 22:40:49 -0800

    Cors filter fix

--- portfolioApi/src/main/java/com/benco/portfolio/util/SecurityConfig.java
+++ portfolioApi/src/main/java/com/benco/portfolio/util/SecurityConfig.java
@@ -7,13 +7,18 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -35,7 +40,7 @@ public class SecurityConfig {
 				.authorizeHttpRequests(authorizeHttpRequests ->
 					authorizeHttpRequests.requestMatchers("/addCustomer")
 						.permitAll()
-				)
+				).cors(cors -> cors.configurationSource(corsConfigurationSource()))
 				.authorizeHttpRequests(authorizeHttpRequests ->
 					authorizeHttpRequests.requestMatchers("/**")
 						.authenticated()
@@ -49,6 +54,14 @@ public class SecurityConfig {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+
+	 @Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+	        return source;
+	    }
 
 	@Bean
 	AuthenticationProvider authenticationProvider() {
